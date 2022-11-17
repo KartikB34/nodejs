c27424fb33822285b23e06d9c27b30f70c008c69
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-11-17 16:46:05 +0530

    removed localhost from consolelog

--- http.js
+++ http.js
@@ -27,5 +27,5 @@ const server = http.createServer((req,res) => {
 
 server.listen(PORT, () => {
 
-    console.log(`Server is working on http://${hostname}:${PORT}`)
+    console.log(`Server is working`)
 })
